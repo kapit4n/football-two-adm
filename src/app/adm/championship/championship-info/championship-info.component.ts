69955f82e5657bb627e14ef93ae6fc989f9770fe
Luis Arce <luis.arce22@gmail.com>
2018-12-31 19:48:31 -0400

    Update save redirection and load data from db

--- src/app/adm/championship/championship-info/championship-info.component.ts
+++ src/app/adm/championship/championship-info/championship-info.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from "@angular/router";
+import { ChampionshipsService } from '../../../services/adm/championships.service'
 
 @Component({
   selector: 'app-championship-info',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChampionshipInfoComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+  data: any;
+  constructor(private champSvc: ChampionshipsService, private route: ActivatedRoute, private router: Router) {
+    this.data = {};
+  }
 
   ngOnInit() {
+    let id = this.route.snapshot.paramMap.get("id");
+    if (id) {
+      this.id = id;
+      this.champSvc.getById(id).subscribe(data => {
+        this.data = data;
+      })
+    }
   }
 
 }
