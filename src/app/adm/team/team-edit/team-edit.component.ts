401dce355cd1fce4964ff1beebc08fbfdb5e447d
Luis Arce <luis.arce22@gmail.com>
2019-01-03 09:24:34 -0400

    Save a team

--- src/app/adm/team/team-edit/team-edit.component.ts
+++ src/app/adm/team/team-edit/team-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { TeamsService } from '../../../services/adm/teams.service'
+import { ActivatedRoute } from '@angular/router';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-team-edit',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamEditComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+  cForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private teamsSvc: TeamsService, private route: ActivatedRoute, private router: Router) {
+    this.cForm = new FormGroup({
+      name: new FormControl(''),
+      banner: new FormControl(''),
+      description: new FormControl('')
+    });
   }
 
+  ngOnInit() {
+    let id = this.route.snapshot.paramMap.get("id");
+    if (id != 'new') {
+      this.id = id;
+      this.teamsSvc.getById(id).subscribe(data => {
+        this.cForm.setValue({ name: data.name, description: data.description, banner: data.banner });
+      })
+    }
+  }
+
+  onSubmit() {
+    let data = {};
+    data = Object.assign({}, this.cForm.value);
+    if (this.id) {
+      data = Object.assign(data);
+      this.teamsSvc.update(this.id, data).subscribe(data => {
+        this.router.navigate(['/team-info/' + this.id]);
+      });
+    } else {
+      this.teamsSvc.save(data).subscribe(data => {
+        this.router.navigate(['/team-list']);
+      });
+    }
+  }
 }
