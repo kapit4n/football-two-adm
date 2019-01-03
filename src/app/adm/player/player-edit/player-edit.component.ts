dd277606a67c24805f800448f66aa56ccc55b90a
Luis Arce <luis.arce22@gmail.com>
2019-01-03 10:58:56 -0400

    Save a player

--- src/app/adm/player/player-edit/player-edit.component.ts
+++ src/app/adm/player/player-edit/player-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { PlayersService } from '../../../services/adm/players.service'
+import { ActivatedRoute } from '@angular/router';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-player-edit',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerEditComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+  cForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private playersSvc: PlayersService, private route: ActivatedRoute, private router: Router) {
+    this.cForm = new FormGroup({
+      name: new FormControl(''),
+      img: new FormControl(''),
+      biography: new FormControl('')
+    });
   }
 
+  ngOnInit() {
+    let id = this.route.snapshot.paramMap.get("id");
+    if (id != 'new') {
+      this.id = id;
+      this.playersSvc.getById(id).subscribe(data => {
+        this.cForm.setValue({ name: data.name, biography: data.biography, img: data.img });
+      })
+    }
+  }
+
+  onSubmit() {
+    let data = {};
+    data = Object.assign({}, this.cForm.value);
+    if (this.id) {
+      data = Object.assign(data);
+      this.playersSvc.update(this.id, data).subscribe(data => {
+        this.router.navigate(['/player-info/' + this.id]);
+      });
+    } else {
+      this.playersSvc.save(data).subscribe(data => {
+        this.router.navigate(['/player-list']);
+      });
+    }
+  }
 }
