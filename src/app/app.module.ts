c1cf0c2bee79fd1980e30f7188a8a2ae2a33c171
Luis Arce <luis.arce22@gmail.com>
2019-01-03 10:27:23 -0400

    Adding match and player components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,13 @@ import { ChampionshipListComponent } from './adm/championship/championship-list/
 import { ChampionshipInfoComponent } from './adm/championship/championship-info/championship-info.component';
 import { TeamEditComponent } from './adm/team/team-edit/team-edit.component';
 import { TeamComponent } from './adm/team/team/team.component';
-import { TeamListComponent } from './adm/team/team-list/team-list.component'
+import { TeamListComponent } from './adm/team/team-list/team-list.component';
+import { PlayerComponent } from './adm/player/player/player.component';
+import { PlayerEditComponent } from './adm/player/player-edit/player-edit.component';
+import { PlayerListComponent } from './adm/player/player-list/player-list.component';
+import { MatchComponent } from './adm/match/match/match.component';
+import { MatchEditComponent } from './adm/match/match-edit/match-edit.component';
+import { MatchListComponent } from './adm/match/match-list/match-list.component'
 
 const appRoutes: Routes = [
   { path: 'championship/:id', component: ChampionshipComponent },
@@ -22,7 +28,14 @@ const appRoutes: Routes = [
   { path: 'championship-list', component: ChampionshipListComponent },
   { path: 'team/:id', component: TeamEditComponent },
   { path: 'team-info/:id', component: TeamComponent },
-  { path: 'team-list', component: TeamListComponent }
+  { path: 'team-list', component: TeamListComponent },
+  { path: 'player/:id', component: PlayerEditComponent },
+  { path: 'player-info/:id', component: PlayerComponent },
+  { path: 'player-list', component: PlayerListComponent },
+  { path: 'match/:id', component: MatchEditComponent },
+  { path: 'match-info/:id', component: MatchComponent },
+  { path: 'match-list', component: MatchListComponent }
+  
 ];
 
 @NgModule({
@@ -33,7 +46,13 @@ const appRoutes: Routes = [
     ChampionshipInfoComponent,
     TeamEditComponent,
     TeamComponent,
-    TeamListComponent
+    TeamListComponent,
+    PlayerComponent,
+    PlayerEditComponent,
+    PlayerListComponent,
+    MatchComponent,
+    MatchEditComponent,
+    MatchListComponent
   ],
   imports: [
     BrowserModule,
